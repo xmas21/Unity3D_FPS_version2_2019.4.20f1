a1f4a28fc1825c7d2cbad8c23b2fd56270a79a5f
HWChia <66512878+xmas21@users.noreply.github.com>
2021-07-07 21:28:07 +0800

    系統新增
    角色簡單移動
    角色簡單轉鏡頭
    角色動畫(5組)

---
+++ FPS/Assets/Scripts/BasePerson.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class BasePerson : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 10)]
+    public float move_speed = 0.2f;
+    [Header("鏡頭旋轉速度"), Range(0, 1000)]
+    public float turn_speed = 3;
+    [Header("跳躍高度"), Range(0, 1000)]
+    public float jump = 100;
+    [Header("血量"), Range(0, 1000)]
+    public float hp = 100;
+    [Header("攻擊力"), Range(0, 500)]
+    public float attack = 20;
+
+    private float hpMax; // 最大血量
+    private Animator ani;
+    private Rigidbody rig;
+    private AudioSource aud;
+
+    private void Start()
+    {
+        GetC();
+    }
+
+    private void GetC() // 抓取元件
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+        aud = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    /// <param name="movePosition">要移動到的位置</param>
+    public void Move(Vector3 movePosition)
+    {
+        rig.MovePosition(transform.position + movePosition * move_speed);
+    }
+
+    /// <summary>
+    /// 鏡頭旋轉
+    /// </summary>
+    /// <param name="turnValue">旋轉值</param>
+    public void Turn(Vector3 turnValue)
+    {
+        transform.Rotate(turnValue * turn_speed);
+    }
+}
---
+++ FPS/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 玩家控制腳本
+/// </summary>
+public class ControllerPlayer : MonoBehaviour
+{
+    private Vector3 v3Move; // 移動的目標位置
+    private Vector3 v3Turn; // 旋轉的目標位置
+    private BasePerson basePerson;
+
+    private void Start()
+    {
+        basePerson = GetComponent<BasePerson>();
+    }
+
+    private void Update()
+    {
+        GetMoveInput();
+        GetTurnInput();
+    }
+
+    private void FixedUpdate() //有跟鋼體有關的都要採用FixedUpdate
+    {
+        basePerson.Move(v3Move);
+        basePerson.Turn(v3Turn);
+    }
+
+    private void GetMoveInput() // 偵測鍵盤輸入
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        v3Move.x = h;
+        v3Move.z = v;
+    }
+
+    private void GetTurnInput() // 偵測滑鼠旋轉輸入
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        v3Turn.x = -mouseY;
+        v3Turn.y = mouseX;
+    }
+}
